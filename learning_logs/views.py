4e6fde86b9fcd8f36a942fa61ba6af0a7a17669b
nuclearwinter88 <nuclearwinter88@gmail.com>
2022-05-02 22:15:16 +0300

    edit entry

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 
 from django.shortcuts import render, redirect
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 
@@ -40,6 +40,7 @@ def new_topic(request):
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+
 def new_entry(request, topic_id):
     """"Добавляет новую запись по конкретной теме"""
     topic = Topic.objects.get(id=topic_id)
@@ -58,3 +59,26 @@ def new_entry(request, topic_id):
     # Вывести пустую или недействительную форму
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    """"Редактирует существующую запись"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # Исходный запрос, форма заполняется данными текущей записи
+        form = EntryForm(instance=entry)
+    else:
+        # Отправлен POST запрос, обработать данные
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic.id)
+
+    context = {
+        'entry': entry,
+        'topic': topic,
+        'form': form,
+    }
+    return render(request, 'learning_logs/edit_entry.html', context)
