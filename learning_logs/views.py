d466a014efedc51c1a53824ee3abf0b660bdc14c
nuclearwinter88 <nuclearwinter88@gmail.com>
2022-05-02 18:41:46 +0300

    добавлена схема url topic.html

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from .models import Topic
+
 
 def index(request):
     """Домашняя страница приложения Learing Log"""
     return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """"Выводит список тем"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    """Выводит тему и связанные записи"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-data_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
